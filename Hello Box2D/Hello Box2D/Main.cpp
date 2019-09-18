b95a39aa2d8fdaf6b9b63b6525042c44561bd0e5
paufiol <45202003+paufiol@users.noreply.github.com>
2019-09-18 19:41:05 +0200

    Box2D Setup (not working)

---
+++ Hello Box2D/Hello Box2D/Main.cpp
@@ -0,0 +1,15 @@
+//#include "Box2D/Box2D.lib"
+#include "Box2D/Box2D.h"
+
+//#include <stdio.h>
+
+
+
+int main(int argc, char** argv) {
+
+	b2Vec2 gravity(0.0f, -10.0f);
+
+	b2World world(gravity);
+	
+	return 0;
+}
