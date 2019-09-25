eaacd076dcd3335e4ddcac839600747f38f0859d
OuEsquerra <paupe200@gmail.com>
2019-09-25 17:57:15 +0200

    main function box2D + SDL2 not working

--- Hello Box2D/Hello Box2D/Main.cpp
+++ Hello Box2D/Hello Box2D/Main.cpp
@@ -1,13 +1,16 @@
 //#include "Box2D/Box2D.lib"
 #include "Box2D/Box2D.h"
 #include "SDL/include/SDL.h"
+
+
 //#include <stdio.h>
-#pragma comment ( lib, "Box2D/Box2D.lib")
 #pragma comment ( lib, "SDL/libx86/SDL2.lib")
 #pragma comment ( lib, "SDL/libx86/SDL2main.lib")
+#pragma comment ( lib, "Box2D/Box2D.lib")
 
 
-int main(int argc, char* argv[]) //int argc, char* argv[]
+
+int main(int argc, char **argv) //int argc, char* argv[]
 {
 
 	//b2Vec2 gravity(0.0f, -10.0f);
