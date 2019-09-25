57bc301089dfe517eb6386c5a3d51eca0423b39f
OuEsquerra <paupe200@gmail.com>
2019-09-25 17:45:00 +0200

    Adding SDL

--- Hello Box2D/Hello Box2D/Main.cpp
+++ Hello Box2D/Hello Box2D/Main.cpp
@@ -1,15 +1,19 @@
 //#include "Box2D/Box2D.lib"
 #include "Box2D/Box2D.h"
-
+#include "SDL/include/SDL.h"
 //#include <stdio.h>
 #pragma comment ( lib, "Box2D/Box2D.lib")
+#pragma comment ( lib, "SDL/libx86/SDL2.lib")
+#pragma comment ( lib, "SDL/libx86/SDL2main.lib")
 
 
-int main(int argc, char** argv) {
+int main(int argc, char* argv[]) //int argc, char* argv[]
+{
 
-	b2Vec2 gravity(0.0f, -10.0f);
+	//b2Vec2 gravity(0.0f, -10.0f);
 
-	b2World world(gravity);
+	//b2World world(gravity);
 	
+	system("pause");
 	return 0;
 }
